640e77f262ccf40c52ad67a4d64fdbc2aff54a96
OsamuChuuya <osamuchuuya26@gmail.com>
2024-12-28 18:12:40 +0500

    Add files via upload

---
+++ script.js
@@ -0,0 +1,9 @@
+let arr = [2, 4, 9, 10, 45, 67, 8, 90]
+console.log(arr, "boshida")
+let even=[]
+for(let i=0; i<8; i++){
+    if(arr[i]%2==0){
+        even.push(arr[i]);
+    }
+}
+console.log( even,` juft sonlarga ajratilgan `) 
